3b29185819a97a25d0df99701e4096440da11e8f
Alain Gadient <alain.gadient@gmail.com>
2022-10-31 09:58:47 +0100

    week2 plot.py is included and the others are deleted

--- week2/plot.py
+++ week2/plot.py
@@ -1,7 +1,13 @@
+
 import pandas as pd
 import matplotlib.pyplot as plt
-df = pd.read_csv("coding-environment-exercise.csv")
 
-plt.figure()
-df.plot()
+df = pd.read_csv("coding-environment-exercise.csv")
+df[0:]
+
+plt.plot(df["date"],df["value"])
+plt.xticks(rotation=90)
 plt.show()
+
+
+
